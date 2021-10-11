c5dfc1f388fafaa18d514c9039ea7541911564b9
pradyumn mittal <mittalpradyumn@gmail.com>
2021-10-11 22:19:22 +0530

    ADD: Sections for the Tesla

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Dilevery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Dilevery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Dilevery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Dilevery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs less Than a New Roof Plus Solar Panels"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
